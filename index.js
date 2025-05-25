c811d93f013760fac5c64768bc91427b2e482682
halukonuss <halukonus3@gmail.com>
2025-05-25 16:04:46 +0300

    Update index.js

--- index.js
+++ index.js
@@ -5,10 +5,12 @@ const axios = require("axios");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.set("trust proxy", true);
+app.set("trust proxy", true); // Render gibi platformlarda IP'yi doğru almak için
 
 app.get("/", async (req, res) => {
-  const ip = req.headers["x-forwarded-for"] || req.socket.remoteAddress;
+  const forwarded = req.headers["x-forwarded-for"];
+  const ip = forwarded ? forwarded.split(",")[0] : req.connection.remoteAddress;
+
   const message = `Yeni Ziyaretçi IP: ${ip}`;
   const url = `https://api.telegram.org/bot${process.env.BOT_TOKEN}/sendMessage`;
 
